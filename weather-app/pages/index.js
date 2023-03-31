3fa4839d47b822b2ab8fee3c65273a98fd642f22
Guillermo L <Guillermo.lucia94@gmail.com>
2023-03-31 12:38:35 +0200

    Carga Arquetipo básico

---
+++ weather-app/pages/index.js
@@ -0,0 +1,15 @@
+
+
+import homeStyles from '@/pages/Home.module.css'
+
+
+
+export default function Home() {
+  return (
+    <>
+      <main className={homeStyles.main}>
+        ejemplo
+      </main>
+    </>
+  )
+}
